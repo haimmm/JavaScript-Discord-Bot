3c5745ebf4980d7b6a40f32573d72941354488f5
haimmm <40215987+haimmm@users.noreply.github.com>
2019-09-17 02:57:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-const token = 'NjIwNTk4NTM5NjM0MDgxNzkz.XYARew.w4pVqNyFPJLg_K-9MvY1uqUFVtc';
-const prefix = '!';
-//const ytdl = require("ytdL-core");
-const baladog = "219527193200427008";
-const balabot = "620598539634081793";
-const delay = new Set();
+    const Discord = require('discord.js');
+    const bot = new Discord.Client();
+    const token = 'NjIwNTk4NTM5NjM0MDgxNzkz.XYAeQg.nD0PJJDWVLbUWi7HWA3l99xOpFA';
+    const prefix = '!';
 
-bot.on('message', message=>{
-    let args = message.content.substring(prefix.length).split(" ");
+    bot.on('ready', () =>{
+        console.log('This bot is online!');
+    })
+    
+    bot.on('message', message=>{
+        let args = message.content.substring(prefix.length).split(" ");
+    
+        if(args[0] === 'join'){
+            message.reply("i'm working");            
+        
+        
+             const channel = bot.channels.get("618149617464508431");
+             if (!channel) return console.error("The channel does not exist!");
 
-    if(args[0] === 'join'){
-        message.reply("i'm working");            
-    
-    
-        const channel = bot.channels.get("618149617464508431");
-         if (!channel) return console.error("The channel does not exist!");
-         channel.join().then(connection => {
-                 message.reply("inside vc"); 
-         }).catch(e => {
-                console.error(e);
-         }); 
-    }
-}) 
+             channel.join().then(connection => {
+                   message.reply("entered vc");
+                   const dispatcher = connection.playFile('music.mp3'); // PLAY FROM LOCAL FILE
+                  dispatcher.on("end", function(){
+                      connection.disconnect(); // DC AFTER
+                  }); 
+             }).catch(e => {
+                  console.error(e);
+            }); 
+         }
+    }) 
 
 bot.login(process.env.BOT_TOKEN);
